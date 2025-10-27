fdc1a205ca545a024642697acd07f37e64fb86dd
Andy Sun <zcbtunx@ucl.ac.uk>
2025-10-27 11:34:52 +0000

    Initialize parameterized testing for tesst1

--- test_times.py
+++ test_times.py
@@ -2,8 +2,6 @@ import pytest
 from times import time_range, compute_overlap_time
 
 
-
-
 def test_given_input():
     large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
     short = time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60)
@@ -40,3 +38,14 @@ def test_starts_at_end():
 def test_ends_before_start():
      with pytest.raises(ValueError, match="end_time must be after start_time"):
         time_range("2022-01-01 12:00:00", "2022-01-01 10:00:00")
+
+
+@pytest.mark.parametrize("time_range1,time_range2,expected", [
+    (time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00"),
+     time_range("2010-01-12 10:30:00", "2010-01-12 10:45:00", 2, 60), 
+     [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), 
+        ('2010-01-12 10:38:00', '2010-01-12 10:45:00')])，
+        ])
+def test_eval(time_range1, time_range2, expected):
+    assert compute_overlap_time(time_range1, time_range2) == expected
+        
